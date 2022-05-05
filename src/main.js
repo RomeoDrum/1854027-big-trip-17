65ac8aaffc8bad62d9909e53f966b5b124bfda80
Romeo <Lakers.08@mail.ru>
2022-05-05 20:41:15 +0300

    Время добивать камни

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import TripInfoView from './view/trip-info-view';
 import TripFiltersView from './view/trip-filters-view';
 import TripSortView from './view/trip-sort-view';
 import { render } from './render.js';
@@ -7,8 +8,10 @@ const siteMainElement = document.querySelector('.page-main');
 const tripFiltersElement = document.querySelector('.trip-controls__filters');
 const tripSortElement = document.querySelector('.trip-events');
 const boardPresenter = new BoardPresenter();
+const tripMainElement = document.querySelector('.trip-main');
 
 render (new TripFiltersView(), tripFiltersElement);
 render (new TripSortView, tripSortElement);
+render (new TripInfoView, tripMainElement);
 
 boardPresenter.init(siteMainElement);
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,22 +1,20 @@
 import TripEventEditView from '../view/trip-event-edit-view.js';
 import TripEventListView  from '../view/trip-event-item.js';
-import TripFiltersView from '../view/trip-filters-view.js';
-import TripSortView from '../view/trip-sort-view.js';
-
+import TripEventItemView from '../view/trip-event-item.js';
 import {render} from '../render.js';
 
 export default class BoardPresenter {
   editFormComponent = new TripEventEditView();
-  tripFiltersComponent = new TripFiltersView();
-  tripSortComponent = new TripSortView();
+  tripEventListComponent = new TripEventListView();
 
   init = (container) => {
     this.container = container;
 
-    render(this.editFormComponent, this.container);
+    render(this.tripEventListComponent, this.container);
+    render(this.editFormComponent, this.tripEventListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
-      render(new TripEventListView (), this.container);
+      render(new TripEventItemView (), this.container);
     }
 
   };
