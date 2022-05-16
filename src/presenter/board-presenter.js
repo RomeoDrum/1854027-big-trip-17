dfe80397ea8ebc6a5ca8a2914eb4be563ab30b1a
Romeo <Lakers.08@mail.ru>
2022-05-16 19:13:03 +0300

    Шаблонизируй это

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -2,7 +2,6 @@ import TripEventEditView from '../view/trip-event-edit-view.js';
 import TripEventListView  from '../view/trip-event-list-view';
 import TripEventItemView from '../view/trip-event-item.js';
 import {render} from '../render.js';
-import PointsModel from '../model/points-model.js';
 
 export default class BoardPresenter {
   editFormComponent = new TripEventEditView();
@@ -12,8 +11,6 @@ export default class BoardPresenter {
     this.container = container;
     this.pointsModel = pointsModel;
     this.destinationPoints = [this.pointsModel.getPoints()];
-
-    console.log(this.destinationPoints);
 
     render(this.tripEventListComponent, this.container);
     render(this.editFormComponent, this.tripEventListComponent.getElement());
